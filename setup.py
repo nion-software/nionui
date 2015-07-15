cea93035d822e47dac2f3afbb1dc7ace9be1a18f
Chris Meyer <cmeyer1969@gmail.com>
2015-07-15 10:01:29 -0700

    Tweaks to get nionui to build under Windows.

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import setuptools
 import os
-from setuptools import setup, find_packages
 
-# Utility function to read the README file.
+# for some reason os gets munged after this point on Windows, so compute it here.
+readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md")
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(
-    name = "nion.ui",
-    version = "0.0.1",
-    long_description=read('README.md'),
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+setuptools.setup(
+    name="nion.ui",
+    version="0.0.1",
+    long_description=open(readme_path).read(),
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['scipy', 'numpy'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha"
     ],
-    include_package_data = True,
-    package_data = {
+    include_package_data=True,
+    package_data={
         'nion.ui': ['*.html'],
     },
     test_suite="nion.ui.test"

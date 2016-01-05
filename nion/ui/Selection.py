f2552b987fd8d5ce17eb81757926f41cfcc6e30e
Chris Meyer <cmeyer1969@gmail.com>
2016-01-05 15:43:29 -0800

    Add option to set selection style on IndexedSelection object.

--- nion/ui/Selection.py
+++ nion/ui/Selection.py
@@ -6,6 +6,7 @@ A collection of useful classes for handling selections.
 from __future__ import absolute_import
 
 # standard libraries
+import enum
 import numbers
 
 # third party libraries
@@ -15,12 +16,20 @@ import numbers
 from . import Event
 
 
+class Style(enum.Enum):
+    none = 0
+    single = 1
+    single_or_none = 2
+    multiple = 3
+
+
 class IndexedSelection(object):
-    def __init__(self):
+    def __init__(self, selection_style=None):
         super(IndexedSelection, self).__init__()
         self.__changed_event = Event.Event()
         self.__indexes = set()
         self.__anchor_index = None
+        self.selection_style = selection_style if selection_style else Style.multiple
 
     @property
     def changed_event(self):
@@ -39,8 +48,12 @@ class IndexedSelection(object):
 
     def clear(self):
         old_index = self.__indexes.copy()
-        self.__indexes = set()
-        self.__anchor_index = None
+        if self.selection_style == Style.single:
+            self.__indexes.add(0)
+            self.__anchor_index = 0
+        else:
+            self.__indexes = set()
+            self.__anchor_index = None
         if old_index != self.__indexes:
             self.__changed_event.fire()
 
@@ -51,38 +64,56 @@ class IndexedSelection(object):
 
     def add(self, index):
         assert isinstance(index, numbers.Integral)
-        old_index = self.__indexes.copy()
-        self.__indexes.add(index)
-        if len(old_index) == 0:
-            self.__anchor_index = index
-        if old_index != self.__indexes:
-            self.__changed_event.fire()
+        if self.selection_style in (Style.single, Style.single_or_none):
+            self.set(index)
+        elif self.selection_style in (Style.multiple, ):
+            old_index = self.__indexes.copy()
+            self.__indexes.add(index)
+            if len(old_index) == 0:
+                self.__anchor_index = index
+            if old_index != self.__indexes:
+                self.__changed_event.fire()
+        else:
+            self.clear()
 
     def remove(self, index):
         assert isinstance(index, numbers.Integral)
-        old_index = self.__indexes.copy()
-        self.__indexes.remove(index)
-        if not self.__anchor_index in self.__indexes:
-            self.__update_anchor_index()
-        if old_index != self.__indexes:
-            self.__changed_event.fire()
+        if self.selection_style in (Style.multiple, Style.single_or_none):
+            old_index = self.__indexes.copy()
+            self.__indexes.remove(index)
+            if not self.__anchor_index in self.__indexes:
+                self.__update_anchor_index()
+            if old_index != self.__indexes:
+                self.__changed_event.fire()
+        elif self.selection_style in (Style.single, ):
+            pass
+        else:
+            self.clear()
 
     def set_multiple(self, indexes):
-        old_index = self.__indexes.copy()
-        self.__indexes = set()
-        self.__indexes.update(indexes)
-        self.__anchor_index = list(indexes)[0] if len(indexes) > 0 else None
-        if old_index != self.__indexes:
-            self.__changed_event.fire()
+        if self.selection_style in (Style.multiple, ):
+            old_index = self.__indexes.copy()
+            self.__indexes = set()
+            self.__indexes.update(indexes)
+            self.__anchor_index = list(indexes)[0] if len(indexes) > 0 else None
+            if old_index != self.__indexes:
+                self.__changed_event.fire()
+        elif self.selection_style in (Style.single, Style.single_or_none) and len(indexes) == 1:
+            self.set(list(indexes)[0])
+        else:
+            self.clear()
 
     def set(self, index):
         assert isinstance(index, numbers.Integral)
-        old_index = self.__indexes.copy()
-        self.__indexes = set()
-        self.__indexes.add(index)
-        self.__anchor_index = index
-        if old_index != self.__indexes:
-            self.__changed_event.fire()
+        if self.selection_style in (Style.multiple, Style.single, Style.single_or_none):
+            old_index = self.__indexes.copy()
+            self.__indexes = set()
+            self.__indexes.add(index)
+            self.__anchor_index = index
+            if old_index != self.__indexes:
+                self.__changed_event.fire()
+        else:
+            self.clear()
 
     def toggle(self, index):
         assert isinstance(index, numbers.Integral)
@@ -93,15 +124,20 @@ class IndexedSelection(object):
 
     def extend(self, index):
         assert isinstance(index, numbers.Integral)
-        old_index = self.__indexes.copy()
-        if index > self.__anchor_index:
-            for new_index in range(self.__anchor_index, index + 1):
-                self.__indexes.add(new_index)
-        elif index < self.__anchor_index:
-            for new_index in range(index, self.__anchor_index + 1):
-                self.__indexes.add(new_index)
-        if old_index != self.__indexes:
-            self.__changed_event.fire()
+        if self.selection_style in (Style.multiple, ):
+            old_index = self.__indexes.copy()
+            if index > self.__anchor_index:
+                for new_index in range(self.__anchor_index, index + 1):
+                    self.__indexes.add(new_index)
+            elif index < self.__anchor_index:
+                for new_index in range(index, self.__anchor_index + 1):
+                    self.__indexes.add(new_index)
+            if old_index != self.__indexes:
+                self.__changed_event.fire()
+        elif self.selection_style in (Style.single, Style.single_or_none):
+            self.set(index)
+        else:
+            self.clear()
 
     def insert_index(self, new_index):
         new_indexes = set()

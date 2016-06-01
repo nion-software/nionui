7b1639b6b2278eec1ff0a8e174975c6635489e1e
Chris Meyer <cmeyer1969@gmail.com>
2016-06-01 09:24:30 -0700

    Add option to pass data_fn rather than data to bitmap cell.
    Evaluates at paint time rather than at call time.

--- nion/ui/CanvasItem.py
+++ nion/ui/CanvasItem.py
@@ -2578,25 +2578,25 @@ class TwistDownCanvasItem(CellCanvasItem):
 
 class BitmapCell(object):
 
-    def __init__(self, rgba_bitmap_data=None, background_color=None, border_color=None):
-        super(BitmapCell, self).__init__()
-        self.__rgba_bitmap_data = rgba_bitmap_data
+    def __init__(self, rgba_bitmap_data_fn=None, background_color=None, border_color=None):
+        super().__init__()
+        self.__rgba_bitmap_data_fn = rgba_bitmap_data_fn
         self.__background_color = background_color
         self.__border_color = border_color
         self.update_event = Event.Event()
 
-    def set_rgba_bitmap_data(self, rgba_bitmap_data, trigger_update=True):
-        self.__rgba_bitmap_data = rgba_bitmap_data
+    def set_rgba_bitmap_data_fn(self, rgba_bitmap_data_fn, trigger_update=True):
+        self.__rgba_bitmap_data_fn = rgba_bitmap_data_fn
         if trigger_update:
             self.update_event.fire()
 
     @property
-    def rgba_bitmap_data(self):
-        return self.__rgba_bitmap_data
+    def rgba_bitmap_data_fn(self):
+        return self.__rgba_bitmap_data_fn
 
-    @rgba_bitmap_data.setter
-    def rgba_bitmap_data(self, value):
-        self.set_rgba_bitmap_data(value, trigger_update=True)
+    @rgba_bitmap_data_fn.setter
+    def rgba_bitmap_data_fn(self, value):
+        self.set_rgba_bitmap_data_fn(value, trigger_update=True)
 
     @property
     def background_color(self):
@@ -2637,7 +2637,7 @@ class BitmapCell(object):
 
         rect_args = rect[0][1], rect[0][0], rect[1][1], rect[1][0]
 
-        bitmap_data = self.rgba_bitmap_data
+        bitmap_data = self.rgba_bitmap_data_fn() if self.rgba_bitmap_data_fn else None
 
         # draw the background
         if background_color:
@@ -2676,10 +2676,13 @@ class BitmapCanvasItem(CellCanvasItem):
 
     def __init__(self, rgba_bitmap_data=None, background_color=None, border_color=None):
         super(BitmapCanvasItem, self).__init__()
-        self.cell = BitmapCell(rgba_bitmap_data, background_color, border_color)
+        self.cell = BitmapCell(lambda: rgba_bitmap_data, background_color, border_color)
+
+    def set_rgba_bitmap_data_fn(self, rgba_bitmap_data_fn, trigger_update=True):
+        self.cell.set_rgba_bitmap_data_fn(rgba_bitmap_data_fn, trigger_update)
 
     def set_rgba_bitmap_data(self, rgba_bitmap_data, trigger_update=True):
-        self.cell.set_rgba_bitmap_data(rgba_bitmap_data, trigger_update)
+        self.set_rgba_bitmap_data_fn(lambda: rgba_bitmap_data, trigger_update)
 
     @property
     def rgba_bitmap_data(self):
@@ -2687,7 +2690,7 @@ class BitmapCanvasItem(CellCanvasItem):
 
     @rgba_bitmap_data.setter
     def rgba_bitmap_data(self, rgb_bitmap_data):
-        self.cell.rgba_bitmap_data = rgb_bitmap_data
+        self.cell.rgba_bitmap_data_fn = lambda: rgb_bitmap_data
 
     @property
     def background_color(self):

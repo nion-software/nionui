4d1a0df6702f93302936680701edf3f8d1af206b
Chris Meyer <cmeyer1969@gmail.com>
2016-08-24 09:28:01 -0700

    Add a start_mouse_tracker to facilitate capturing of mouse in popup.

--- nion/ui/MouseTrackingCanvasItem.py
+++ nion/ui/MouseTrackingCanvasItem.py
@@ -1,5 +1,11 @@
+import asyncio
+import functools
+import sys
+import typing
+
 from nion.ui import CanvasItem
 from nion.utils import Geometry
+
 
 class TrackingCanvasItem(CanvasItem.CanvasItemComposition):
 
@@ -49,5 +55,59 @@ class TrackingCanvasItem(CanvasItem.CanvasItemComposition):
         self.root_container.release_mouse()
 
     def grabbed_mouse_position_changed(self, dx, dy, modifiers):
-        self.on_mouse_position_changed_by(dx, dy)
+        self.on_mouse_position_changed_by(Geometry.IntPoint(x=dx, y=dy))
         return True
+
+
+def start_mouse_tracker(ui, event_loop: asyncio.AbstractEventLoop, canvas_item: CanvasItem.AbstractCanvasItem,
+                        mouse_position_changed_by_fn: typing.Callable[[Geometry.IntPoint], None], global_pos: Geometry.IntPoint, size: Geometry.IntSize):
+
+    tracking_canvas_item = TrackingCanvasItem()
+    tracking_canvas_item.on_mouse_position_changed_by = mouse_position_changed_by_fn
+    tracking_canvas_item.add_canvas_item(canvas_item)
+
+    async def close_window(document_window):
+        document_window.request_close()
+
+    def handle_close(document_window):
+        tracking_canvas_item.release_mouse()
+        event_loop.create_task(close_window(document_window))
+
+    def activation_changed(document_window, activated):
+        if not activated:
+            handle_close(document_window)
+
+    # create the popup window
+    document_window = ui.create_document_window()
+    document_window.window_style = "mousegrab"
+
+    document_window.on_activation_changed = functools.partial(activation_changed, document_window)
+    tracking_canvas_item.on_close = functools.partial(handle_close, document_window)
+
+    # configure canvas widget, attach to document window
+    mousegrab_window_pos = global_pos - Geometry.IntPoint(x=size.width, y=size.height / 2)
+    document_window.show(size=size, position=mousegrab_window_pos)
+    if sys.platform == "win32":
+        relative_pos = Geometry.IntPoint()
+    else:
+        relative_pos = mousegrab_window_pos
+        document_window.fill_screen()
+    document_window.fill_screen()
+    canvas_widget = ui.create_canvas_widget()
+    tracking_canvas_item.sizing.set_fixed_size(size)
+    content_row_canvas_item = CanvasItem.CanvasItemComposition()
+    content_row_canvas_item.layout = CanvasItem.CanvasItemRowLayout()
+    content_row_canvas_item.add_spacing(relative_pos.x)
+    content_row_canvas_item.add_canvas_item(tracking_canvas_item)
+    content_row_canvas_item.add_stretch()
+    content_canvas_item = CanvasItem.CanvasItemComposition()
+    content_canvas_item.layout = CanvasItem.CanvasItemColumnLayout()
+    content_canvas_item.add_spacing(relative_pos.y)
+    content_canvas_item.add_canvas_item(content_row_canvas_item)
+    content_canvas_item.add_stretch()
+    canvas_widget.canvas_item.add_canvas_item(content_canvas_item)
+    document_window.attach(canvas_widget)
+    tracking_canvas_item.request_focus()
+    tracking_canvas_item.cursor_shape = "blank"
+    canvas_widget.set_cursor_shape("blank")
+    tracking_canvas_item.grab_mouse(relative_pos.x + size.width // 2, relative_pos.y + size.height // 2)

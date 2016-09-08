a24b93fdbc862521038caf1689a615d91a4a4ecc
Chris Meyer <cmeyer1969@gmail.com>
2016-09-08 07:14:21 -0700

    Update example to use widgets.

--- examples/basic/HelloWorld.py
+++ examples/basic/HelloWorld.py
@@ -63,43 +63,14 @@ class HelloWorldWindow(Window.Window):
     def __init__(self, ui, app=None):
         super().__init__(ui, app)
 
-        # first create a root canvas item in which the rest of the user interface will go
-        canvas_widget = ui.create_canvas_widget()
-
-        background_canvas_item = CanvasItem.BackgroundCanvasItem("#FFF")
-
-        # create the hello world text and size it to its contents
-        hello_world_canvas_item = CanvasItem.CanvasItemComposition()
-        hello_world_str = _("Hello World!")
-        hello_world_button = CanvasItem.TextButtonCanvasItem(hello_world_str)
-        hello_world_button.font = "normal 15px sans-serif"
-        hello_world_button.size_to_content(ui.get_font_metrics)
-        hello_world_canvas_item.add_canvas_item(hello_world_button)
-        hello_world_canvas_item.sizing.copy_from(hello_world_button.sizing)
+        hello_world_str_ref = ["Hello World!", ]
 
         # place the hello world text into a row so that the text can be left justified within the row
         # by adding a stretch on the right side of the row. then configure the height of the row to match
         # the height of the hello world text.
-        # TODO: be able to position item within row directly via add_canvas_item
-        text_row_canvas_item = CanvasItem.CanvasItemComposition()
-        text_row_canvas_item.layout = CanvasItem.CanvasItemRowLayout()
-        # text_row_canvas_item.add_spacing(1.0)
-        text_row_canvas_item.add_canvas_item(hello_world_canvas_item)
-        text_row_canvas_item.add_stretch()
-        text_row_canvas_item.sizing.set_fixed_height(hello_world_canvas_item.sizing.maximum_height)
 
         # make a column in which to place the hello world text and a custom canvas item, add the items,
         # then add stretch at the bottom so that the fixed height items get pushed to the top.
-        column_canvas_item = CanvasItem.CanvasItemComposition()
-        column_canvas_item.layout = CanvasItem.CanvasItemColumnLayout(spacing=12)
-        column_canvas_item.add_canvas_item(text_row_canvas_item)
-        brown_square_row = CanvasItem.CanvasItemComposition()
-        brown_square_row.layout = CanvasItem.CanvasItemRowLayout(spacing=4)
-        brown_square_canvas_item = BrownSquareCanvasItem()
-        brown_square_row.add_canvas_item(brown_square_canvas_item)
-        brown_square_row.add_stretch()
-        brown_square_row.sizing.set_fixed_height(brown_square_canvas_item.sizing.preferred_height)
-        column_canvas_item.add_canvas_item(brown_square_row)
         progress_bar_row = CanvasItem.CanvasItemComposition()
         progress_bar_row.layout = CanvasItem.CanvasItemRowLayout(spacing=4)
         progress_bar_canvas_item = CanvasItem.ProgressBarCanvasItem()
@@ -116,28 +87,54 @@ class HelloWorldWindow(Window.Window):
         check_box_row.add_canvas_item(check_box_canvas_item)
         check_box_row.add_stretch()
         check_box_row.sizing.set_fixed_height(20)
+        column_canvas_item = CanvasItem.CanvasItemComposition()
+        column_canvas_item.layout = CanvasItem.CanvasItemColumnLayout(spacing=12, alignment="start")
+        brown_square_row = CanvasItem.CanvasItemComposition()
+        brown_square_row.layout = CanvasItem.CanvasItemRowLayout(spacing=4)
+        brown_square_canvas_item = BrownSquareCanvasItem()
+        brown_square_row.add_canvas_item(brown_square_canvas_item)
+        brown_square_row.add_stretch()
+        brown_square_row.sizing.set_fixed_height(brown_square_canvas_item.sizing.preferred_height)
+        column_canvas_item.add_canvas_item(brown_square_row)
         column_canvas_item.add_canvas_item(progress_bar_row)
         column_canvas_item.add_canvas_item(check_box_row)
         column_canvas_item.add_stretch()
-
-        # finally add the column to the root canvas item.
-        canvas_widget.canvas_item.add_canvas_item(background_canvas_item)
-        canvas_widget.canvas_item.add_canvas_item(column_canvas_item)
-
-        # attach the root canvas item to the document window
-        self.attach_widget(canvas_widget)
 
         # configure what happens when the button is pressed
         click_count_ref = [0]
         def button_clicked():
             click_count_ref[0] += 1
-            hello_world_button.text = hello_world_str + " (" + str(click_count_ref[0]) + ")"
-            # TODO: support some sort of auto sizing to make the code below automatic
-            hello_world_button.size_to_content(ui.get_font_metrics)
-            hello_world_canvas_item.sizing.copy_from(hello_world_button.sizing)
-            text_row_canvas_item.refresh_layout()
+            hello_widget.text = hello_world_str_ref[0] + " (" + str(click_count_ref[0]) + ")"
             progress_bar_canvas_item.progress += 0.1
-        hello_world_button.on_button_clicked = button_clicked
+
+        def checked_changed(value):
+            hello_world_str_ref[0] = "Goodbye World..." if value else "Hello World!"
+            button_clicked()
+
+        # finally add the column to a root widget and attach the root widget to the document window.
+        root_widget = ui.create_column_widget(alignment="start")
+
+        one_row = ui.create_row_widget()
+        one_row.add_stretch()
+        one_row.add(ui.create_label_widget("Centered"))
+        one_row.add_stretch()
+
+        check_box_widget = ui.create_check_box_widget("Check BOX")
+
+        hello_widget = ui.create_push_button_widget(hello_world_str_ref[0])
+
+        canvas_widget = ui.create_canvas_widget(properties={"height": 500, "width": 600})
+        canvas_widget.canvas_item.add_canvas_item(column_canvas_item)
+
+        root_widget.add(one_row)
+        root_widget.add(check_box_widget)
+        root_widget.add(hello_widget)
+        root_widget.add(canvas_widget)
+
+        self.attach_widget(root_widget)
+
+        hello_widget.on_clicked = button_clicked
+        check_box_widget.on_checked_changed = checked_changed
 
 
 def main(args, bootstrap_args):

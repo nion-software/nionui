819f2fb5cbbe00e40ffb843646e2068a9518ed08
Chris Meyer <cmeyer1969@gmail.com>
2018-05-11 09:02:26 -0700

    Use tool to launch command rather than duplicating code.

--- nion/ui/command.py
+++ nion/ui/command.py
@@ -71,22 +71,9 @@ def main():
 
     # first attempt to launch using nionui-launcher
     if pkg_resources.Environment()["nionui-tool"]:
-        if sys.platform == "darwin":
-            app_path = pkg_resources.resource_filename("nion.nionui_tool", "macosx/Nion UI Launcher.app")
-            exe_path = os.path.join(app_path, "Contents", "MacOS", "Nion UI Launcher")
-        elif sys.platform == "linux":
-            app_dir = pkg_resources.resource_filename("nion.nionui_tool", "linux")
-            exe_path = os.path.join(app_dir, "NionUILauncher")
-        elif sys.platform == "win32":
-            app_dir = pkg_resources.resource_filename("nion.nionui_tool", "windows")
-            exe_path = os.path.join(app_dir, "NionUILauncher.exe")
-        else:
-            exe_path = None
-        if exe_path:
-            python_prefix = os.sys.prefix
-            proc = subprocess.Popen([exe_path, python_prefix] + sys.argv[1:], universal_newlines=True)
-            proc.communicate()
-            return
+        from nion.nionui_tool import command
+        command.launch(sys.argv)
+        return
 
     # next attempt to launch using pyqt
     success = False

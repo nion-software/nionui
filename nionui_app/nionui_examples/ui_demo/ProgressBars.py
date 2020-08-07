48b0507f5aeccf3cce7e4e8e02473dbab516dfb9
Alberto Eljarrat Ascunce <aeljarrat@physik.hu-berlin.de>
2020-08-07 10:36:03 +0200

    fix progress bar label needed converter

--- nionui_app/nionui_examples/ui_demo/ProgressBars.py
+++ nionui_app/nionui_examples/ui_demo/ProgressBars.py
@@ -1,5 +1,5 @@
 from nion.utils import Model
-
+from nion.utils import Converter
 
 class Handler:
 
@@ -8,15 +8,16 @@ class Handler:
     def reset(self, widget):
         self.slider_value_model.value = 50
 
+    slider_converter = Converter.IntegerToStringConverter('{0}')
+
 
 def construct_ui(ui):
 
     slider = ui.create_slider(value="@binding(slider_value_model.value)")
-
-    progress_bar = ui.create_progress_bar(value="@binding(slider_value_model.value)")
-
-    label = ui.create_label(text="@binding(slider_value_model.value)")
-
+    progress_bar = ui.create_progress_bar(
+        value="@binding(slider_value_model.value)")
+    label = ui.create_label(
+        text="@binding(slider_value_model.value, converter=slider_converter)")
     button = ui.create_push_button(text="Reset to 50", on_clicked="reset")
-
+    
     return ui.create_column(progress_bar, slider, label, button, spacing=8)

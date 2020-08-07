5d94178b30ac8059f8181d5b72d30c7683b2887d
Alberto Eljarrat Ascunce <aeljarrat@physik.hu-berlin.de>
2020-08-07 10:56:14 +0200

    fix groups label also needed converter

--- nionui_app/nionui_examples/ui_demo/Groups.py
+++ nionui_app/nionui_examples/ui_demo/Groups.py
@@ -1,4 +1,5 @@
 from nion.utils import Model
+from nion.utils import Converter
 
 
 class Handler:
@@ -8,14 +9,18 @@ class Handler:
     def reset(self, widget):
         self.slider_value_model.value = 50
 
+    slider_converter = Converter.IntegerToStringConverter('{0}')
+
 
 def construct_ui(ui):
 
     slider = ui.create_slider(value="@binding(slider_value_model.value)")
 
-    progress_bar = ui.create_progress_bar(value="@binding(slider_value_model.value)")
+    progress_bar = ui.create_progress_bar(
+        value="@binding(slider_value_model.value)")
 
-    label = ui.create_label(text="@binding(slider_value_model.value)")
+    label = ui.create_label(
+        text="@binding(slider_value_model.value, converter=slider_converter)")
 
     button = ui.create_push_button(text="Reset to 50", on_clicked="reset")
 

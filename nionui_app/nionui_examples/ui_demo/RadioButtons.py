0a2953ce96da1a48f8724bbaaa167a04eaef156d
Alberto Eljarrat Ascunce <aeljarrat@physik.hu-berlin.de>
2020-08-07 10:15:37 +0200

    fix radio buttons label needed a converter

--- nionui_app/nionui_examples/ui_demo/RadioButtons.py
+++ nionui_app/nionui_examples/ui_demo/RadioButtons.py
@@ -1,4 +1,5 @@
 from nion.utils import Model
+from nion.utils import Converter
 
 class Handler:
     radio_button_value = Model.PropertyModel(2)
@@ -7,11 +8,17 @@ class Handler:
     def reset_clicked(self, widget):
         self.radio_button_value.value = 2
 
+    radio_converter = Converter.IntegerToStringConverter('{0}')
+
 
 def construct_ui(ui):
-    radio0 = ui.create_radio_button(text="One", value=1, group_value="@binding(radio_button_value.value)")
-    radio1 = ui.create_radio_button(text="Two", value=2, group_value="@binding(radio_button_value.value)")
-    radio2 = ui.create_radio_button(text="Three", value=3, group_value="@binding(radio_button_value.value)")
-    label = ui.create_label(text="@binding(radio_button_value.value)")
+    radio0 = ui.create_radio_button(
+        text="One", value=1, group_value="@binding(radio_button_value.value)")
+    radio1 = ui.create_radio_button(
+        text="Two", value=2, group_value="@binding(radio_button_value.value)")
+    radio2 = ui.create_radio_button(
+        text="Three", value=3, group_value="@binding(radio_button_value.value)")
+    label = ui.create_label(
+        text="@binding(radio_button_value.value, converter=radio_converter)")
     button = ui.create_push_button(text="Reset", on_clicked="reset_clicked")
     return ui.create_column(radio0, radio1, radio2, label, button, spacing=8)

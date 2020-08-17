75e9c827074640ea9d16ac96063401d3bfcd98e7
Chris Meyer <cmeyer1969@gmail.com>
2020-08-17 15:24:01 -0700

    Fix formatting in Window.py.

--- nion/ui/Window.py
+++ nion/ui/Window.py
@@ -342,10 +342,12 @@ class Window:
 
     def register_dialog(self, dialog: Window) -> None:
         old_on_close = dialog.on_close
+
         def close_dialog():
             self.__dialogs.remove(weakref.ref(dialog))
             if callable(old_on_close):
                 old_on_close()
+
         dialog.on_close = close_dialog
         self.__dialogs.append(weakref.ref(dialog))
 

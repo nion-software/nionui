e91285ac4c504cd0a7e07c80b24b4cd69c07d91a
Chris Meyer <cmeyer1969@gmail.com>
2021-08-09 11:22:59 -0700

    Add another line edit example with binding.

--- nionui_app/nionui_examples/ui_demo/LineEdits.py
+++ nionui_app/nionui_examples/ui_demo/LineEdits.py
@@ -1,6 +1,10 @@
+from nion.utils import Model
+
 class Handler:
 
-    color_line_edit = None
+    def __init__(self):
+        self.color_line_edit = None
+        self.model = Model.PropertyModel("red")
 
     def color_updated(self, widget, text):
         print(f"Color changed: {widget} {text}")
@@ -18,4 +22,10 @@ class Handler:
 def construct_ui(ui):
     field_label = ui.create_label(text="Favorite Color?")
     field_line_edit = ui.create_line_edit(placeholder_text="Color", name="color_line_edit", on_editing_finished="color_updated", on_return_pressed="return_pressed", on_key_pressed="key_pressed")
-    return ui.create_row(ui.create_spacing(8), field_label, ui.create_spacing(8), field_line_edit, ui.create_spacing(8))
+    field_line_edit2 = ui.create_line_edit(text="@binding(model.value)")
+    return ui.create_column(
+        ui.create_row(field_label, field_line_edit, spacing=8),
+        ui.create_row(ui.create_label(text="Another Color?"), field_line_edit2, ui.create_stretch(), spacing=8),
+        ui.create_stretch(),
+        spacing=8,
+    )

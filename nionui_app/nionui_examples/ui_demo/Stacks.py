b5e5d21fb5041c4a3f862050bec78fa79d8a2a1c
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2026-09-02 11:15:59 -0700

    Improve stacks page spacing in demo

--- nionui_app/nionui_examples/ui_demo/Stacks.py
+++ nionui_app/nionui_examples/ui_demo/Stacks.py
@@ -7,9 +7,9 @@ class Handler(Declarative.Handler):
 
 
 def construct_ui(u: Declarative.DeclarativeUI) -> Declarative.UIDescription:
-    stack0 = u.create_column(u.create_label(text="111"), u.create_label(text="ONE"))
-    stack1 = u.create_column(u.create_label(text="222"), u.create_label(text="TWO"))
-    stack2 = u.create_column(u.create_label(text="333"), u.create_label(text="THREE"))
+    stack0 = u.create_column(u.create_label(text="111"), u.create_label(text="ONE"), spacing=8)
+    stack1 = u.create_column(u.create_label(text="222"), u.create_label(text="TWO"), spacing=8)
+    stack2 = u.create_column(u.create_label(text="333"), u.create_label(text="THREE"), spacing=8)
     stack = u.create_stack(stack0, stack1, stack2, current_index="@binding(stack_index_model.value)")
     chooser = u.create_combo_box(items=["One", "Two", "Three"], current_index="@binding(stack_index_model.value)")
-    return u.create_column(stack, chooser, spacing=8)
+    return u.create_column(stack, chooser, u.create_stretch(), spacing=8)

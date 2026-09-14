345c3d25915c361f7ce2cd88daf44a5b1a9caa02
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2026-09-14 16:10:07 -0700

    Add a list view demo page to the ui demo application.

--- nionui_app/nionui_examples/ui_demo/main.py
+++ nionui_app/nionui_examples/ui_demo/main.py
@@ -25,6 +25,7 @@ from . import Groups
 from . import Layout
 from . import LineEdits
 from . import ListBoxes
+from . import ListViews
 from . import ProgressBars
 from . import RadioButtons
 from . import ScrollAreas
@@ -72,6 +73,7 @@ def main(args: typing.Sequence[typing.Any], bootstrap_args: typing.Mapping[str, 
         (Groups, "groups", _("Groups")),
         (LineEdits, "line_edits", _("Line Edits")),
         (ListBoxes, "list_boxes", _("List Boxes")),
+        (ListViews, "list_views", _("List Views")),
         (ProgressBars, "progress_bars", _("Progress Bars")),
         (RadioButtons, "radio_buttons", _("Radio Buttons")),
         (ScrollAreas, "scroll_areas", _("Scroll Areas")),

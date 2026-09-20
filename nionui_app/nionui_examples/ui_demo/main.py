64ae041a8516df90418579edb9be8160bf486676
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2026-09-19 17:29:53 -0700

    Demonstrate drag and drop between two list views in the ui demo.

--- nionui_app/nionui_examples/ui_demo/main.py
+++ nionui_app/nionui_examples/ui_demo/main.py
@@ -24,6 +24,7 @@ from . import ComponentStack
 from . import ContextMenus
 from . import Converters
 from . import Dialogs
+from . import DragAndDrop
 from . import FocusKeyboard
 from . import Groups
 from . import Layout
@@ -83,6 +84,7 @@ def main(args: typing.Sequence[typing.Any], bootstrap_args: typing.Mapping[str, 
         (ContextMenus, "context_menus", _("Context Menus")),
         (Converters, "converters", _("Converters")),
         (Dialogs, "dialogs", _("Dialogs")),
+        (DragAndDrop, "drag_and_drop", _("Drag and Drop")),
         (FocusKeyboard, "focus_keyboard", _("Focus and Keyboard")),
         (Groups, "groups", _("Groups")),
         (LineEdits, "line_edits", _("Line Edits")),
